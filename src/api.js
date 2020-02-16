defb591b9350bb0ddc46e90878ebba0ca373cab9
Arun Abraham <arun4xp@gmail.com>
2020-02-16 16:45:03 +0530

    fix warnings

--- src/api.js
+++ src/api.js
@@ -1,11 +1,10 @@
-const data = require('./data.json');
 const stopWords = require('./stop-words.json');
 
 function getKeyWords(text, stopwords) {
-    const keywords = text.split(/\W+/).
-        map(keyword => keyword.toLowerCase()).
-        filter((token) => {
-            return token.length >= 2 && stopwords.indexOf(token) == -1;
+    const keywords = text.split(/\W+/)
+        .map(keyword => keyword.toLowerCase())
+        .filter((token) => {
+            return token.length >= 2 && stopwords.indexOf(token) === -1;
         });
     return keywords;
 }
