651548c6f1cc7285e3c3c734360b32d157487337
Arun Abraham <arun4xp@gmail.com>
2020-02-18 19:35:50 +0530

    improve naming

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -2,26 +2,24 @@ import React from 'react';
 import './Books.css';
 
 function Books(props) {
-    const renderSelectedBooks = (books) => {
-        if (books.length === 0) {
-            return <div>No books selected</div>
-        }
-        const selectedBooks = books.map((book, index) => {
-            return (
-                <div key={index} className="book-card shadow">
-                    <h3 id="book-title">{book.title}</h3>
-                    <div id="summary" className="gray-text">{book.summary}</div>
-                    <hr className="gray-line" />
-                    <div id="author" className="gray-text">{book.author}</div>
-                </div>
-            )
-        })
-        return selectedBooks;
+    const books = props.books;
+    if (books.length === 0) {
+        return <div>No books selected</div>
     }
 
+    const cards = books.map((book, index) => {
+        return (
+            <div key={index} className="book-card shadow">
+                <h3 id="book-title">{book.title}</h3>
+                <div id="summary" className="gray-text">{book.summary}</div>
+                <hr className="gray-line" />
+                <div id="author" className="gray-text">{book.author}</div>
+            </div>
+        )
+    })
     return (
-        <div className="selected-books">
-            {renderSelectedBooks(props.books)}
+        <div className="books">
+            {cards}
         </div>
     );
 }
