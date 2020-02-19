305c59ea1331e51ffb6ee481677a3c8c462559b3
Arun Abraham <arun4xp@gmail.com>
2020-02-19 10:42:56 +0530

    add more test specs

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -14,3 +14,24 @@ it("when we enter a keyword, it should display some suggestions", () => {
     })
     expect(wrapper.find("li")).toHaveLength(3);
 })
+
+it("should clear suggestions on selecting one", () => {
+    const wrapper = mount(<App />);
+    wrapper.find("input[type='text']").simulate("change", {
+        target: { value: 'achieve' }
+    })
+    wrapper.find("li").at(0).text()
+    wrapper.find("li").at(0).simulate("click")
+    expect(wrapper.find("li")).toHaveLength(0);
+})
+
+it("should replace the search term on selecting a suggestion", () => {
+    const wrapper = mount(<App />);
+    wrapper.find("input[type='text']").simulate("change", {
+        target: { value: 'achieve' }
+    })
+    wrapper.find("li").at(0).text()
+    wrapper.find("li").at(0).simulate("click")
+    const inputValue = wrapper.find("input[type='text']").get(0).props.value;
+    expect(inputValue).toEqual('The Richest Man in Babylon')
+})
